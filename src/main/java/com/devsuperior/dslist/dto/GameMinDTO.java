1dfceebcca29c7e75b7cfa06ffd6374e36790a2f
devpedroferreira <pedroofan@gmail.com>
2025-01-22 10:15:33 -0300

    create class GameController and test http-GET ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -22,6 +22,26 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
 	
 	
 }
