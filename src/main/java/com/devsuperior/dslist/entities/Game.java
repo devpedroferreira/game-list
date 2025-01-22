05bc2360a5d7de450584f1f8576192688c950c5e
devpedroferreira <pedroofan@gmail.com>
2025-01-21 23:42:41 -0300

    Created file improt.sql, Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -28,8 +28,7 @@ public class Game {
 	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game() {
-		
+	public Game() {	
 	}
 
 	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
