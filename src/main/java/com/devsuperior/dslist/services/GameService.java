539f0d81649a4bda3a310f50309ba4c56d747cf0
devpedroferreira <pedroofan@gmail.com>
2025-01-23 10:57:36 -0300

    Mapping GameDTO in GameController

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -17,13 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
 		return dto;
 	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 			List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
